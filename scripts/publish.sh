3c21c7a047fb773c5c8c61516bf974afa1d4e715
Connor Szczepaniak <cszczepaniak@gmail.com>
2021-04-04 23:25:19 -0500

    try it this way?:

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,7 +1,7 @@
 set -ex
 
-cd app/
-dotnet publish src/OhHellBackend/ -c Development
-cd src/OhHellBackend/bin/Development/netcoreapp3.1/publish/
+cd app/src/OhHellBackend/
+dotnet publish -c Development
+cd bin/Development/netcoreapp3.1/publish/
 zip -r $GITHUB_SHA.zip .
 aws s3 cp $GITHUB_SHA.zip s3://oh-hell-backend-artifacts/
