d4bbb029bef882d8df17553221663c8d8e93c7de
Connor Szczepaniak <cszczepaniak@gmail.com>
2021-04-04 23:17:59 -0500

    use zip better

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,5 +2,6 @@ set -ex
 
 cd app/
 dotnet publish src/OhHellBackend/ -c Development
-zip -r $GITHUB_SHA.zip src/OhHellBackend/bin/Development/netcoreapp3.1/publish/
+cd src/OhHellBackend/bin/Development/netcoreapp3.1/publish/
+zip -r $GITHUB_SHA.zip .
 aws s3 cp $GITHUB_SHA.zip s3://oh-hell-backend-artifacts/
