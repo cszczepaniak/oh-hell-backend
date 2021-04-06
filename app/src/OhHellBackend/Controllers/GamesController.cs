226dda6e0387238fb1cf0b91bf0cdeea1a5e1394
Connor Szczepaniak <cszczepaniak@gmail.com>
2021-04-05 20:38:08 -0500

    rename and remove unused endpoints

---
+++ app/src/OhHellBackend/Controllers/GamesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OhHellBackend.Controllers
+{
+    [Route("api/[controller]")]
+    public class GamesController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Save()
+        {
+            return Ok();
+        }
+    }
+}
