329bf9b71f4695970d95dc86f7bea53d5eb253f6
Connor Szczepaniak <cszczepaniak@gmail.com>
2021-05-10 23:44:08 -0500

    don't use reserved names

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -25,9 +25,6 @@ export class InfrastructureStack extends cdk.Stack {
       environment: {
         BUCKET: dataBucket.bucketName,
         GIN_MODE: "release",
-        AWS_ACCESS_KEY_ID: process.env["AWS_ACCESS_KEY_ID"] ?? "",
-        AWS_SECRET_ACCESS_KEY: process.env["AWS_SECRET_ACCESS_KEY"] ?? "",
-        AWS_REGION: process.env["AWS_REGION"] ?? "",
       },
     });
 
