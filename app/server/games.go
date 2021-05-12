d53a24eb0871135d349645021bf428cc427706e9
Connor Szczepaniak <cszczepaniak@gmail.com>
2021-05-11 23:06:17 -0500

    adding one last thing to try

--- app/server/games.go
+++ app/server/games.go
@@ -29,8 +29,12 @@ func (s *Server) handleSaveGame(c *gin.Context) {
 	// 	c.String(400, `error saving game: %s`, err)
 	// 	return
 	// }
+	c.Header("Content-Type", "application/json")
 	c.Header("Access-Control-Allow-Origin", "*")
-	c.String(200, `game saved: %d`, 123)
+	c.Header("Allow", "GET, OPTIONS, POST")
+	c.Header("Access-Control-Allow-Methods", "GET, OPTIONS, POST")
+	c.Header("Access-Control-Allow-Headers", "*")
+	c.Status(200)
 }
 
 func (s *Server) handleGetGame(c *gin.Context) {
